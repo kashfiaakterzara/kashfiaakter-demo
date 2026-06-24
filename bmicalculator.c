962f7658b4699f37d37577866520adce5bb84c1c
kashfiaakterzara <kashfiaakter13@gmail.com>
2026-06-25 01:32:51 +0600

    Update bmicalculator.c
    added if else conditional statements

--- bmicalculator.c
+++ bmicalculator.c
@@ -11,5 +11,19 @@ int main(){
     bmi = (weight/(height * height));
 
     printf("Your BMI is %f", bmi);
+
+    if(bmi<18.5) {
+        printf("U are underweight");
+    }
+    else if(bmi>=18.5 && bmi <25.0){
+    printf("You are healthy");
+    }
+    else if(bmi>=25.0 && bmi< 30.0){
+    printf("You are Overweight");
+    }
+    else
+    {
+    printf("You are obese");    
+    }
     return 0;
 }
